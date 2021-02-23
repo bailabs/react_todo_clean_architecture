3d09bf9f076b631abd1620af38e78c7fdf4a0632
Ivan Ray Altomera <irayspacii@gmail.com>
2021-02-23 15:21:43 +0800

    feat: delete todo list

--- src/app/pages/todos/components/TodoList.tsx
+++ src/app/pages/todos/components/TodoList.tsx
@@ -1,18 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useRecoilValue } from "recoil"
+import { useRecoilState } from "recoil"
 import todosState from "app/atoms/todos"
 
-function TodoDetail({ name }: { name: string }) {
-    return <li className="border border-gray-200 rounded-lg p-4 mb-4">{name}</li>
+function TodoDetail({ name, onClick }: { name: string; onClick: React.MouseEventHandler<HTMLLIElement> }) {
+    return (
+        <li
+            className="border border-gray-200 rounded-lg p-4 mb-4 cursor-pointer hover:text-blue-600 hover:bg-blue-100 hover:border-transparent: hover:shadow-lg"
+            onClick={onClick}
+        >
+            {name}
+        </li>
+    )
 }
 
 export default function TodoList() {
-    const todos = useRecoilValue(todosState)
+    const [todos, setTodos] = useRecoilState(todosState)
+    function handleClick(id: number) {
+        const newTodos = todos.filter((todo) => todo.id !== id)
+        setTodos(newTodos)
+    }
     return (
         <ul>
             {todos.map((todo, i) => (
-                <TodoDetail key={i} name={todo.name} />
+                <TodoDetail key={i} name={todo.name} onClick={() => handleClick(todo.id)} />
             ))}
         </ul>
     )
